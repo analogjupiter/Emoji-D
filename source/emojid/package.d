a51a347fd6cc1d0cf2eeb7b5848d0018ada58b74
0xEAB <desisma@heidel.beer>
2018-09-03 05:14:07 +0200

    Add emoji lib code

---
+++ source/emojid/package.d
@@ -0,0 +1,12 @@
+module emojid;
+
+public:
+import emojid.activities;
+import emojid.animalsandnature;
+import emojid.flags;
+import emojid.foodanddrink;
+import emojid.objects;
+import emojid.people;
+import emojid.smileys;
+import emojid.symbols;
+import emojid.travelandplaces;
